6f7195aed381176f6cf9f0b4f37795519782463e
Kim <skjy7200@gamil.com>
2025-05-26 17:19:56 +0900

    홈 페이지 구성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 // 1페이지 화면 구성
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function Home() {
+  const [quote, setQuote] = useState("");
   const navigate = useNavigate();
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(`https://api.adviceslip.com/advice?timestamp=${Date.now()}`);
+      const data = await response.json();
+      setQuote(data.slip.advice);
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+    const interval = setInterval(fetchQuote, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="home-container">
@@ -14,6 +31,10 @@ function Home() {
         <button className="button" onClick={() => navigate("/quote")}>
           오늘의 명언 뽑기
         </button>
+
+        <div className="scrolling-quote">
+          <p>{quote}</p>
+        </div>
       </div>
     </div>
   );
